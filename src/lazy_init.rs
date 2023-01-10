5c64edb9b37c289f6d910fb2eb07217e3bfb452f
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-10 11:40:11 +1100

    revert to the original implementation of the lazy static code (the new one was not helping with the size of anything at all)

--- src/lazy_init.rs
+++ src/lazy_init.rs
@@ -1,49 +1,56 @@
-macro_rules! lazy_static {
-    ($visibility:vis static ref $name:ident : $type:ty = $initializer:expr ;) => {
-        #[allow(non_camel_case_types)]
-        $visibility struct $name {
-    value: core::cell::UnsafeCell<Option<$type>>,
+use core::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+};
+
+pub struct LazilyInitialized<'initializer, T: Sized + Sync> {
+    value: UnsafeCell<Option<T>>,
+    initializer: &'initializer dyn Fn() -> T,
 }
 
-impl $name {
-    const fn new() -> Self {
+impl<'initializer, T: Sized + Sync> LazilyInitialized<'initializer, T> {
+    pub const fn new(initializer: &'initializer dyn Fn() -> T) -> Self {
         Self {
-            value: core::cell::UnsafeCell::new(None),
+            value: UnsafeCell::new(None),
+            initializer,
         }
     }
 
-    $visibility fn is_initialized(value: &Self) -> bool {
+    pub fn is_initialized(value: &Self) -> bool {
         // TODO: Make this safe. It should be ok for now because we don't have threads yet. Same goes for the rest of these functions.
         unsafe { value.value.get().as_ref().unwrap().is_some() }
     }
 }
 
-impl core::ops::Deref for $name {
-    type Target = $type;
+impl<'initializer, T: Sized + Sync> Deref for LazilyInitialized<'initializer, T> {
+    type Target = T;
 
-    // Needed if the initializer uses an unsafe block (this also hides other warnings but such is life).
-    #[allow(unused_unsafe)]
-    fn deref(&self) -> &$type {
+    fn deref(&self) -> &T {
         // TODO: Make safe (see above)
         unsafe {
             if self.value.get().as_ref().is_none() {
-                *self.value.get() = Some($initializer);
+                *self.value.get() = Some((self.initializer)());
             }
             self.value.get().as_ref().unwrap().as_ref().unwrap()
         }
     }
 }
 
-impl core::ops::DerefMut for $name {
-    fn deref_mut(&mut self) -> &mut $type {
+impl<'initializer, T: Sized + Sync> DerefMut for LazilyInitialized<'initializer, T> {
+    fn deref_mut(&mut self) -> &mut T {
         // TODO: Make safe.
+        unsafe {
             if self.value.get_mut().is_none() {
-                *self.value.get_mut() = Some($initializer);
+                *self.value.get() = Some((self.initializer)());
             }
             self.value.get_mut().as_mut().unwrap()
+        }
     }
 }
-        $visibility static mut $name: $name = $name::new();
+
+macro_rules! lazy_static {
+    ($visibility:vis static ref $name:ident : $type:ty = $initializer:expr ;) => {
+        $visibility static mut $name: crate::lazy_init::LazilyInitialized<$type> = crate::lazy_init::LazilyInitialized::new(&||$initializer);
     };
 }
 
@@ -51,7 +58,7 @@ pub(crate) use lazy_static;
 
 #[cfg(test)]
 mod test {
-    use super::*;
+    use crate::lazy_init::LazilyInitialized;
 
     struct CantConstructAtCompileTime {
         nums: [u8; 24],
@@ -70,9 +77,9 @@ mod test {
     #[test]
     fn lazy_initialization_test() {
         unsafe {
-            assert!(!LAZY_VARIABLE::is_initialized(&LAZY_VARIABLE));
+            assert!(!LazilyInitialized::is_initialized(&LAZY_VARIABLE));
             LAZY_VARIABLE.nums[0] = 56;
-            assert!(LAZY_VARIABLE::is_initialized(&LAZY_VARIABLE));
+            assert!(LazilyInitialized::is_initialized(&LAZY_VARIABLE));
             assert_eq!(LAZY_VARIABLE.nums[0], 56);
             assert_eq!(LAZY_VARIABLE.nums[1], 42);
         }
