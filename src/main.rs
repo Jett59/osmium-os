6a47638cf669f1be9004efdbf90657124f0c020a
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-18 20:27:14 +1100

    completely remove the legacy console interface

--- src/main.rs
+++ src/main.rs
@@ -35,8 +35,6 @@ mod x86_64;
 #[cfg(target_arch = "x86_64")]
 pub use x86_64::arch_api;
 
-use arch_api::console;
-
 // Needed to silence rust-analyzer which uses test mode, where this is unused because the panic handler is conditionally excluded in test mode
 #[allow(unused_imports)]
 use core::panic::PanicInfo;
@@ -46,14 +44,11 @@ extern crate alloc;
 #[panic_handler]
 #[cfg(not(test))]
 fn kpanic(_info: &PanicInfo) -> ! {
-    console::write_string("Panic!!!\n");
     loop {}
 }
 
 #[no_mangle]
 extern "C" fn kmain() -> ! {
-    console::clear();
-    console::write_string("Hello, World!\n");
     arch_api::init::arch_init();
     pmm::sanity_check();
     heap::sanity_check();
