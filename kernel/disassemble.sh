e44f3e5d7195f5faf77aa8c9670503bb8d113b7d
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-21 16:18:15 +1100

    fix a couple small issues with the disassembly of aarch64 and the linker script:

--- kernel/disassemble.sh
+++ kernel/disassemble.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # This rather long sed command prettifies the output of objdump to make it far easier to read the disassembly
-llvm-objdump -C -d -l build/target/osmium | sed -r -e 's/[0-9a-fA-F]{2,}\s*<(.*)>/\1/g' -e 's/\s+/ /g' -e 's/^\s*//g' -e 's/^[0-9a-fA-F]+:\s*(.+)/\1/g' -e 's/^([0-9a-fA-F]{2}\s)+//g' > build/target/disassembly.s
+llvm-objdump -C -d -l build/target/osmium | sed -r -e 's/(0x)?[0-9a-fA-F]{2,}\s*<(.*)>/\2/g' -e 's/\s+/ /g' -e 's/^\s*//g' -e 's/^[0-9a-fA-F]+:\s*(.+)/\1/g' -e 's/^([0-9a-fA-F]{2}\s)+//g' > build/target/disassembly.s
