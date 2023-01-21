69719d80085c65c6ae126750a66d7bc06454203f
Luke Thompson <endeavour9@gmail.com>
2023-01-21 16:28:26 +1100

    add template main from the UEFI crate and target supported architectures

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+#![feature(abi_efiapi)]
+#![allow(stable_features)]
+
+use uefi::prelude::*;
+
+#[entry]
+fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    uefi_services::init(&mut system_table).unwrap();
+
+    Status::SUCCESS
 }
