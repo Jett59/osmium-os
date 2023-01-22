3d0b648236909990d8c7669e5571ea5a7315ac39
Luke Thompson <endeavour9@gmail.com>
2023-01-22 22:58:51 +1100

    implement code to read the config file on boot and display it (still need to figure out the different potential errors - for now unwrapping for non-uefi error methods)

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,46 +3,40 @@
 #![feature(abi_efiapi)]
 #![allow(stable_features)]
 
-use uefi::{prelude::*, proto::{loaded_image::{LoadedImage, self}, device_path::{DevicePath, DevicePathNodeEnum}}, table::boot::ScopedProtocol};
+extern crate alloc;
+use alloc::{vec, string::String};
+use uefi::{prelude::*, proto::{media::file::{FileMode, FileAttribute, FileInfo, File}}};
+use uefi::{Result};
 
 #[entry]
-fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
 
     uefi_services::println!("Hello, Osmium World!");
 
     let boot_services = system_table.boot_services();
-    let loaded_image = boot_services.open_protocol_exclusive::<LoadedImage>(handle).unwrap();
-
-    discover_devices(loaded_image, boot_services);
+    let result = read_config(image, boot_services);
+    if result != Result::Ok(()) {
+        uefi_services::println!("Error: {:?}", result);
+    }
 
     loop {}
 }
 
-fn discover_devices(loaded_image: ScopedProtocol<LoadedImage>, boot_services: &BootServices) {
-    let device_path = boot_services.open_protocol_exclusive::<DevicePath>(loaded_image.device()).unwrap();
-    for device_node in device_path.node_iter() {
-        let device_node_enum = device_node.as_enum().unwrap();
-        match device_node_enum {
-            DevicePathNodeEnum::AcpiAcpi(value)=> {
-                uefi_services::println!("ACPI ACPI {:?}", value);
-            },
-            DevicePathNodeEnum::AcpiAdr(value)=> {
-                uefi_services::println!("ACPI ADR {:?}", value);
-            },
-            DevicePathNodeEnum::AcpiExpanded(value)=> {
-                uefi_services::println!("ACPI Expanded {:?}", value);
-            },
-            DevicePathNodeEnum::AcpiNvdimm(value)=> {
-                uefi_services::println!("ACPI Nvdimm {:?}", value);
-            },
-            _ => {
-                uefi_services::println!("Device Node: {:?}", device_node);
-            }
-        }        
-    }
-    for device_instance in device_path.instance_iter() {
-        uefi_services::println!("Device Instance: {:?}", device_instance);
-    }
+fn read_config(image: Handle, boot_services: &BootServices) -> Result {
+    uefi_services::println!("Reading config file");
+    let mut fs = boot_services.get_image_file_system(image)?;
+    let mut root = fs.open_volume()?;
+    let mut file = root.open(cstr16!("\\boot\\osmium\\boot.toml"), FileMode::Read, FileAttribute::empty())?.into_regular_file().unwrap();
+    
+    let mut info_buffer = vec![0; 128];
+    let file_info = file.get_info::<FileInfo>(&mut info_buffer).unwrap();
+    let file_size = file_info.file_size() as usize;
 
+    let mut buffer = vec![0u8; file_size];
+    let _size = file.read(&mut buffer).unwrap();
+    let config = String::from_utf8_lossy(&buffer);
+    uefi_services::println!("{config}");
+
+    return Ok(());
 }
