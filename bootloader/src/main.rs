21c4c29d7907168c5e32230608ea6c589ad93ee5
Luke Thompson <endeavour9@gmail.com>
2023-01-22 20:52:46 +1100

    pull out device path iteration to a function

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,7 +3,7 @@
 #![feature(abi_efiapi)]
 #![allow(stable_features)]
 
-use uefi::{prelude::*, proto::{loaded_image::{LoadedImage, self}, device_path::{DevicePath}}};
+use uefi::{prelude::*, proto::{loaded_image::{LoadedImage, self}, device_path::{DevicePath, DevicePathNodeEnum}}, table::boot::ScopedProtocol};
 
 #[entry]
 fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
@@ -14,13 +14,35 @@ fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     let boot_services = system_table.boot_services();
     let loaded_image = boot_services.open_protocol_exclusive::<LoadedImage>(handle).unwrap();
 
+    discover_devices(loaded_image, boot_services);
+
+    loop {}
+}
+
+fn discover_devices(loaded_image: ScopedProtocol<LoadedImage>, boot_services: &BootServices) {
     let device_path = boot_services.open_protocol_exclusive::<DevicePath>(loaded_image.device()).unwrap();
-    for device in device_path.node_iter() {
-        uefi_services::println!("Device Node: {:?}", device);
+    for device_node in device_path.node_iter() {
+        let device_node_enum = device_node.as_enum().unwrap();
+        match device_node_enum {
+            DevicePathNodeEnum::AcpiAcpi(value)=> {
+                uefi_services::println!("ACPI ACPI {:?}", value);
+            },
+            DevicePathNodeEnum::AcpiAdr(value)=> {
+                uefi_services::println!("ACPI ADR {:?}", value);
+            },
+            DevicePathNodeEnum::AcpiExpanded(value)=> {
+                uefi_services::println!("ACPI Expanded {:?}", value);
+            },
+            DevicePathNodeEnum::AcpiNvdimm(value)=> {
+                uefi_services::println!("ACPI Nvdimm {:?}", value);
+            },
+            _ => {
+                uefi_services::println!("Device Node: {:?}", device_node);
+            }
+        }        
     }
     for device_instance in device_path.instance_iter() {
         uefi_services::println!("Device Instance: {:?}", device_instance);
     }
 
-    loop {}
 }
