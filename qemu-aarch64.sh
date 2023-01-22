ffb43f773b79256183ccec0da2339206d2733afd
Luke Thompson <endeavour9@gmail.com>
2023-01-22 17:16:27 +1100

    add support for homebrew installed qemu on mac

--- qemu-aarch64.sh
+++ qemu-aarch64.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
-if [ ! -f build/flash0.img ]; then
-    dd if=/dev/zero of=build/flash0.img bs=1M count=64
-    dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of=build/flash0.img conv=notrunc
-fi
+set -e
 
+dd if=/dev/zero of=build/flash0.img bs=1M count=64
+
+LINUX_FILE="/usr/share/qemu-efi-aarch64/QEMU_EFI.fd"
+MAC_BREW_FILE="/opt/homebrew/share/qemu/edk2-aarch64-code.fd"
+for FILE in "${LINUX_FILE}" "${MAC_BREW_FILE}"
+do
+  if [ -f "$FILE" ]; then
+    echo "$FILE exists. Writing to flash0."
+    dd if="$FILE" of=build/flash0.img conv=notrunc
+    break
+  fi
+done
+
+echo "Launching qemu..."
 qemu-system-aarch64 -machine virt -cpu cortex-a57 -drive file=build/flash0.img,format=raw,if=pflash -hda build/osmium.img -device virtio-gpu-pci
