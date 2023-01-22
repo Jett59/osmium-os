51d5baeda699e10a14d402e5524a728d63139967
Luke Thompson <endeavour9@gmail.com>
2023-01-22 18:43:04 +1100

    try reading from the LoadedImage

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,13 +3,19 @@
 #![feature(abi_efiapi)]
 #![allow(stable_features)]
 
-use uefi::prelude::*;
+use uefi::{prelude::*, proto::loaded_image::LoadedImage};
 
 #[entry]
-fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+fn main(handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
 
     uefi_services::println!("Hello, Osmium World!");
+
+    let boot_services = system_table.boot_services();
+    let loaded_image = boot_services.open_protocol_exclusive::<LoadedImage>(handle).unwrap();
+    let (info, info2) = loaded_image.info();
+
+    uefi_services::println!("Loaded Image: ${info2}");
 
     loop {}
 }
