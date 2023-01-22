cd4c476c8e79487405f63fd79f54106e996cae02
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-22 17:20:23 +1100

    add windows option to the qemu-aarch64.sh script

--- qemu-aarch64.sh
+++ qemu-aarch64.sh
@@ -6,7 +6,8 @@ dd if=/dev/zero of=build/flash0.img bs=1M count=64
 
 LINUX_FILE="/usr/share/qemu-efi-aarch64/QEMU_EFI.fd"
 MAC_BREW_FILE="/opt/homebrew/share/qemu/edk2-aarch64-code.fd"
-for FILE in "${LINUX_FILE}" "${MAC_BREW_FILE}"
+WINDOWS_FILE="C:\\Program Files\\qemu\\edk2-aarch64-code.fd"
+for FILE in "${LINUX_FILE}" "${MAC_BREW_FILE}" "${WINDOWS_FILE}"
 do
   if [ -f "$FILE" ]; then
     echo "$FILE exists. Writing to flash0."
