d6a077c44f5c4c827ddb0aab00818e4eb666b55d
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-22 16:42:04 +1100

    update the build-aarch script to create the build directory

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -3,6 +3,8 @@
 set +x
 set -e
 
+mkdir -p build
+
 cd bootloader && ./build.sh && cd ..
 
 # Ref: https://superuser.com/questions/1657478/how-make-a-bootable-iso-for-my-uefi-application-bare-bones#comment2537987_1657538
