683d77db01fc7334f2e8670cad41d3bd7e44db11
Jett Thompson <jett.thompson.59@gmail.com>
2023-01-24 15:14:02 +1100

    make the aarch64 build place the kernel on the file system in addition to the configuration and bootloader

--- build-aarch64.sh
+++ build-aarch64.sh
@@ -10,6 +10,7 @@ cd bootloader && ./build.sh && cd ..
 # Ref: https://superuser.com/questions/1657478/how-make-a-bootable-iso-for-my-uefi-application-bare-bones#comment2537987_1657538
 dd if=/dev/zero of=build/osmium.img bs=48000000 count=1
 mformat -i build/osmium.img ::
+
 mmd -i build/osmium.img ::/EFI
 mmd -i build/osmium.img ::/EFI/BOOT
 mcopy -i build/osmium.img bootloader/build/bootloader.efi ::/EFI/BOOT/BOOTAA64.EFI
@@ -17,3 +18,4 @@ mcopy -i build/osmium.img bootloader/build/bootloader.efi ::/EFI/BOOT/BOOTAA64.E
 mmd -i build/osmium.img ::/boot
 mmd -i build/osmium.img ::/boot/osmium
 mcopy -i build/osmium.img config/boot.toml ::/boot/osmium/boot.toml
+mcopy -i build/osmium.img kernel/build/osmium ::/boot/osmium/osmium
