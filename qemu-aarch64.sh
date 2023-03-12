c1fb31d5f127c4d06fbddb87c4b746e43821c8ab
Jett Thompson <jett.thompson.59@gmail.com>
2023-03-12 14:20:06 +1100

    add the serial console to the virtio

--- qemu-aarch64.sh
+++ qemu-aarch64.sh
@@ -6,7 +6,7 @@ dd if=/dev/zero of=build/flash0.img bs=1M count=64
 
 LINUX_FILE="/usr/share/qemu-efi-aarch64/QEMU_EFI.fd"
 MAC_BREW_FILE="/opt/homebrew/share/qemu/edk2-aarch64-code.fd"
-WINDOWS_FILE="C:\\Program Files\\qemu\\edk2-aarch64-code.fd"
+WINDOWS_FILE="C:\\Program Files\\qemu\\share\\edk2-aarch64-code.fd"
 for FILE in "${LINUX_FILE}" "${MAC_BREW_FILE}" "${WINDOWS_FILE}"
 do
   if [ -f "$FILE" ]; then
@@ -17,4 +17,4 @@ do
 done
 
 echo "Launching qemu..."
-qemu-system-aarch64 -machine virt -cpu cortex-a57 -drive file=build/flash0.img,format=raw,if=pflash -hda build/osmium.img -device virtio-gpu-pci
+qemu-system-aarch64 -machine virt -cpu cortex-a57 -drive file=build/flash0.img,format=raw,if=pflash -hda build/osmium.img -serial stdio -device virtio-gpu $@
