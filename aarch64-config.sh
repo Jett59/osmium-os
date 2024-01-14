faf35cab5bc6280a28f593bdea0c1719239eec5d
Luke Thompson <endeavour9@gmail.com>
2024-01-15 10:58:45 +1100

    add support for mac syntax for sed replace

--- aarch64-config.sh
+++ aarch64-config.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-sed 's/x86_64/aarch64/g' -i .config
+# Detect the OS
+OS="$(uname)"
+
+# Use the appropriate sed command based on the OS
+if [ "$OS" = "Darwin" ]; then
+  # macOS
+  sed -i '' 's/x86_64/aarch64/g' .config
+  sed -i '' 's/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/g' kernel.code-workspace
+else
+  # Linux and others
+  sed 's/x86_64/aarch64/g' -i .config
+  sed 's/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/g' -i kernel.code-workspace
+fi
 sed 's/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/g' -i kernel.code-workspace
--- x86_64-config.sh
+++ x86_64-config.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-sed 's/aarch64/x86_64/g' -i .config
+# Detect the OS
+OS="$(uname)"
+
+# Use the appropriate sed command based on the OS
+if [ "$OS" = "Darwin" ]; then
+  # macOS
+  sed -i '' 's/aarch64/x86_64/g' .config
+  sed -i '' 's/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/g' kernel.code-workspace
+else
+  # Linux and others
+  sed 's/aarch64/x86_64/g' -i .config
+  sed 's/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/g' -i kernel.code-workspace
+fi
 sed 's/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/g' -i kernel.code-workspace
