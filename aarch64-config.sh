da69e72c37bd130eb582c2c31a2167dc3ae9379c
Luke Thompson <endeavour9@gmail.com>
2024-01-18 11:42:37 +1100

    Update aarch64-config.sh

--- aarch64-config.sh
+++ aarch64-config.sh
@@ -13,4 +13,3 @@ else
   sed 's/x86_64/aarch64/g' -i .config
   sed 's/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/g' -i kernel.code-workspace
 fi
-sed 's/"rust-analyzer.cargo.target": "x86_64-unknown-linux-gnu"/"rust-analyzer.cargo.target": "aarch64-unknown-linux-gnu"/g' -i kernel.code-workspace
