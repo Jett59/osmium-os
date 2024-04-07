a21cf425212205be24bda5685adab372ecbb5f6a
jett59 <jett.thompson.59@gmail.com>
2024-04-07 16:41:40 +1000

    change the build-initial-ramdisk.sh file to not use cp -t (mac doesn't support it)

--- build-initial-ramdisk.sh
+++ build-initial-ramdisk.sh
@@ -4,7 +4,7 @@ set +x
 set -e
 
 mkdir -p build/initial_ramdisk
-cp -RT user/build build/initial_ramdisk
+cp -r user/build/* build/initial_ramdisk
 
 cd build/initial_ramdisk
 tar -cf ../initial_ramdisk.tar *
