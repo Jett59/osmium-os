02576d331f6280bb45919430e2b384a0de09b881
Jett Thompson <jett.thompson.59@gmail.com>
2026-07-18 15:29:24 +1000

    remove the unsafe code from the console.rs

--- kernel/src/console.rs
+++ kernel/src/console.rs
@@ -1,6 +1,8 @@
 use core::fmt::Write;
+use core::sync::atomic::{AtomicUsize, Ordering};
 
 use alloc::boxed::Box;
+use alloc::vec;
 use common::font::get_character_dimensions;
 use common::framebuffer::get_screen_dimensions;
 use spin::LazyLock;
@@ -18,93 +20,85 @@ pub fn get_console_dimensions() -> (usize, usize) {
 
 static CONSOLE_BACKBUFFER: LazyLock<spin::Mutex<Box<[char]>>> = LazyLock::new(|| {
     let console_dimensions = get_console_dimensions();
-    spin::Mutex::new(unsafe {
-        Box::new_zeroed_slice(console_dimensions.0 * console_dimensions.1).assume_init()
-    })
+    spin::Mutex::new(vec!['\0'; console_dimensions.0 * console_dimensions.1].into_boxed_slice())
 });
 
-static mut X: usize = 0;
-static mut Y: usize = 0;
+static X: AtomicUsize = AtomicUsize::new(0);
+static Y: AtomicUsize = AtomicUsize::new(0);
 
-fn possibly_scroll() {
-    unsafe {
-        let console_dimensions = get_console_dimensions();
-        if Y >= console_dimensions.1 {
-            let mut console_backbuffer = CONSOLE_BACKBUFFER.lock();
-            let mut lines = console_backbuffer
-                .chunks_exact_mut(console_dimensions.0)
-                .peekable();
-            for row in 1..console_dimensions.1 {
-                let destination = lines.next().unwrap();
-                let source = lines.peek().unwrap();
-                let source_line_length = source
-                    .iter()
-                    .take_while(|&&character| character != '\n')
-                    .count();
-                let old_destination_line_length = destination
-                    .iter()
-                    .take_while(|&&character| character != '\n')
-                    .count();
-                destination[..source_line_length].copy_from_slice(&source[..source_line_length]);
-                // Only insert the \n if the line isn't full.
-                if source_line_length < console_dimensions.0 {
-                    destination[source_line_length] = '\n';
-                }
-                for (x, source_char) in source
-                    .iter()
-                    .enumerate()
-                    // Only really works because source is longer than it needs to be.
-                    .take(usize::max(source_line_length, old_destination_line_length))
-                {
-                    font_renderer::draw_character(
-                        if x < source_line_length {
-                            *source_char
-                        } else {
-                            ' '
-                        },
-                        x * get_character_dimensions().0,
-                        (row - 1) * get_character_dimensions().1,
-                    );
-                }
+fn possibly_scroll(console_backbuffer: &mut [char]) {
+    let console_dimensions = get_console_dimensions();
+    let y = Y.load(Ordering::SeqCst);
+    if y >= console_dimensions.1 {
+        let mut lines = console_backbuffer
+            .chunks_exact_mut(console_dimensions.0)
+            .peekable();
+        for row in 1..console_dimensions.1 {
+            let destination = lines.next().unwrap();
+            let source = lines.peek().unwrap();
+            let source_line_length = source
+                .iter()
+                .take_while(|&&character| character != '\n')
+                .count();
+            let old_destination_line_length = destination
+                .iter()
+                .take_while(|&&character| character != '\n')
+                .count();
+            destination[..source_line_length].copy_from_slice(&source[..source_line_length]);
+            // Only insert the \n if the line isn't full.
+            if source_line_length < console_dimensions.0 {
+                destination[source_line_length] = '\n';
             }
-            // Now clear out the last row.
-            for x in 0..console_dimensions.0 {
-                console_backbuffer[(console_dimensions.1 - 1) * console_dimensions.0 + x] = ' ';
+            for (x, source_char) in source
+                .iter()
+                .enumerate()
+                // Only really works because source is longer than it needs to be.
+                .take(usize::max(source_line_length, old_destination_line_length))
+            {
                 font_renderer::draw_character(
-                    ' ',
+                    if x < source_line_length {
+                        *source_char
+                    } else {
+                        ' '
+                    },
                     x * get_character_dimensions().0,
-                    (console_dimensions.1 - 1) * get_character_dimensions().1,
+                    (row - 1) * get_character_dimensions().1,
                 );
             }
-            Y -= 1;
-            X = 0;
         }
+        // Now clear out the last row.
+        for x in 0..console_dimensions.0 {
+            console_backbuffer[(console_dimensions.1 - 1) * console_dimensions.0 + x] = ' ';
+            font_renderer::draw_character(
+                ' ',
+                x * get_character_dimensions().0,
+                (console_dimensions.1 - 1) * get_character_dimensions().1,
+            );
+        }
+        Y.store(console_dimensions.1 - 1, Ordering::SeqCst);
+        X.store(0, Ordering::SeqCst);
     }
 }
 
 pub fn write_character(character: char) {
-    let x = unsafe { X };
-    let y = unsafe { Y };
-    CONSOLE_BACKBUFFER.lock()[x + y * get_console_dimensions().0] = character;
+    let x = X.load(Ordering::SeqCst);
+    let y = Y.load(Ordering::SeqCst);
+    let mut console_backbuffer = CONSOLE_BACKBUFFER.lock();
+    console_backbuffer[y * get_console_dimensions().0 + x] = character;
     if character == '\n' {
-        unsafe {
-            X = 0;
-            Y += 1;
-            possibly_scroll();
-        }
+        X.store(0, Ordering::SeqCst);
+        Y.store(y + 1, Ordering::SeqCst);
+        possibly_scroll(&mut console_backbuffer);
     } else {
         font_renderer::draw_character(
             character,
             x * get_character_dimensions().0,
             y * get_character_dimensions().1,
         );
-        unsafe {
-            X += 1;
-            if X >= get_console_dimensions().0 {
-                X = 0;
-                Y += 1;
-                possibly_scroll();
-            }
+        if x + 1 >= get_console_dimensions().0 {
+            X.store(0, Ordering::SeqCst);
+            Y.store(y + 1, Ordering::SeqCst);
+            possibly_scroll(&mut console_backbuffer);
         }
     }
 }
